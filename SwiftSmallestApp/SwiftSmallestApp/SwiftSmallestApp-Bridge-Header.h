3704706d9bd71eaf22ecc78d5149b4a3a7d2dde9
Andy <boruil@cs.washington.edu>
2014-07-19 14:31:44 -0700

    changing UI and try to import AFNetworking

---
+++ SwiftSmallestApp/SwiftSmallestApp/SwiftSmallestApp-Bridge-Header.h
@@ -0,0 +1,9 @@
+//
+//  SwiftSmallestApp-Bridge-Header.h
+//  SwiftSmallestApp
+//
+//  Created by Borui "Andy" Li on 7/19/14.
+//  Copyright (c) 2014 Borui "Andy" Li. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
---
+++ SwiftSmallestApp/SwiftSmallestApp_Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  SwiftSmallestApp_Bridging-Header.h
+//  SwiftSmallestApp
+//
+//  Created by Borui "Andy" Li on 7/19/14.
+//  Copyright (c) 2014 Borui "Andy" Li. All rights reserved.
+//
+
